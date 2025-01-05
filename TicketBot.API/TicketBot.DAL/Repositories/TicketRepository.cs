493c43fa88979fc7e02534a1df5b0feeeadc08c9
quintenbosch <r0785961@student.thomasmore.be>
2025-01-05 14:47:43 +0100

    Includes are fixed

--- TicketBot.API/TicketBot.DAL/Repositories/TicketRepository.cs
+++ TicketBot.API/TicketBot.DAL/Repositories/TicketRepository.cs
@@ -1,4 +1,5 @@
-﻿using TicketBot.DAL.Data;
+﻿using System.Linq.Expressions;
+using TicketBot.DAL.Data;
 using TicketBot.DAL.Models;
 
 namespace TicketBot.DAL.Repositories
@@ -6,5 +7,18 @@ namespace TicketBot.DAL.Repositories
     public class TicketRepository : Repository<Ticket>, ITicketRepository
     {
         public TicketRepository(AppDbContext context) : base(context) { }
-    }
+
+		public async Task<IEnumerable<Ticket>> GetAllWithIncludesAsync(params Expression<Func<Ticket, object>>[] includes)
+		{
+			// Add default includes if not passed in the parameters
+			var defaultIncludes = new Expression<Func<Ticket, object>>[]
+			{
+				t => t.Schedule,
+				s => s.Schedule.Movie
+			};
+
+			// Call the base method with merged includes
+			return await base.GetAllAsync(defaultIncludes.Concat(includes).ToArray());
+		}
+	}
 }
